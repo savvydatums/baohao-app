35f432edb30b2c0536d96cf50e69e601e24e74df
Ruby <youshihyu@gmail.com>
2019-07-12 17:10:31 +0800

    add faq page

--- src/pages/dashboard/faq/index.ts
+++ src/pages/dashboard/faq/index.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+
+type TQuesitonPair = {
+	Q: string;
+	A: string;
+};
 
 @IonicPage({ name: "FaqPage", segment: "FaqPage" })
 @Component({
   selector: 'faq',
   templateUrl: 'index.html',
 })
+
 export class FaqPage {
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	questions: TQuesitonPair[];
+
+	constructor(
+		public navCtrl: NavController,
+		public translate: TranslateService, 
+		public navParams: NavParams) {
 	}
 
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad FaqPage');
+	ionViewWillEnter() {
+		this.questions =  this.translate.instant('FAQ')
 	}
 
 }
